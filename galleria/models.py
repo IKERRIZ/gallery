c50a7b398f80c312a6f0e1c6969fdbee8318b60b
IKERRIZ <okothfaith94@gmail.com>
2019-11-09 20:13:07 +0300

    Add delete function to the models

--- galleria/models.py
+++ galleria/models.py
@@ -7,11 +7,11 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
-    class Meta:
-        ordering = ['location']
-
     def save_location(self):
         self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
