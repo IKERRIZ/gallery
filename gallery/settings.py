4fc2f7d5dbd7b25f86f253a539c278951637ae71
IKERRIZ <okothfaith94@gmail.com>
2019-11-11 15:39:45 +0300

    Initial deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOTS = os.path.join(BASE)_DIR,'staticsfiles')
+STATIC_ROOT = os.path.join(BASE)_DIR,'staticsfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static"),
